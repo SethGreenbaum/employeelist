c8751853c17822fa7b56b94deb0d7376d896db2d
Seth Greenbaum <sethmgreenbaum@gmail.com>
2020-05-03 09:04:03 -0600

    cleaning it all up

--- src/components/SearchResultContainer.js
+++ src/components/SearchResultContainer.js
@@ -9,13 +9,6 @@ class SearchResultContainer extends Component {
     results: []
   };
 
-  //
-  // When this component mounts, search the Giphy API for pictures of kittens
-  // This method is built-in to React and automatically runs after the component 
-  // is rendered for the first time. componentDidMount runs after a component and 
-  // all of its children have been initially rendered and mounted to the DOM, 
-  // and have no further expected initialization steps
-  //
   componentDidMount() {
     this.searchList();
   }
@@ -50,7 +43,7 @@ class SearchResultContainer extends Component {
     });
   };
 
-  // When the form is submitted, search the Giphy API for `this.state.search`
+
   handleFormSubmit = event => {
     event.preventDefault();
     this.searchDept(this.state.search);
@@ -62,9 +55,7 @@ class SearchResultContainer extends Component {
     this.sortSalary()
   }
 
-  // render is a lifecycle event, but rather than only running once 
-  // the render method is called every time our component's state is updated or 
-  // anytime our component receives new props.
+
   render() {
     return (
       <div>
@@ -83,17 +74,3 @@ class SearchResultContainer extends Component {
 export default SearchResultContainer;
 
 
-/*
-Design pattern: Containers and Presentation components
-
-1. Container components are primarily concerned with how things work and render very little, 
- any of their own markup. Instead they mostly render other components and pass down the logic 
- and data they need to work.
-
-2. Presentational components are concerned with how things look and typically don't contain 
-any logic that doesn't have to do with their own individual UI.
-
-This pattern helps us build components with little to no coupling that can more easily be reused in different parts of app 
-or even across applications. It also lets us stub out our applications appearance by writing presentational components first, 
-and then writing container components once we're ready to make things work.
-*/
